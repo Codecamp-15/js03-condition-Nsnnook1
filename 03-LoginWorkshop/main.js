c548f02192437554c639821df6c5d7137f1d9578
Nsnnook1 <nooksiri62@gmail.com>
2023-08-08 09:02:20 +0700

    finished_hw03-login

---
+++ 03-LoginWorkshop/main.js
@@ -0,0 +1,13 @@
+let user = prompt("what is your name");
+ if (user === "codecamp") {
+  prompt(Number(password));
+} else if (password = 123456) {
+  user = "codecamp";
+}else if (password != 123456){
+    alert('Wrong password')
+}else if (user != String) {
+    user = "guest";
+    alert("guest");
+  }
+
+alert(user);
