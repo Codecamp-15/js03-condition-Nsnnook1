421009be6af1fed58f94df074463e27de5b51407
Nsnnook1 <nooksiri62@gmail.com>
2023-08-08 08:42:54 +0700

    add solution2 (09)

--- 01-If-Else/09-sortNumber/main.js
+++ 01-If-Else/09-sortNumber/main.js
@@ -1,5 +1,5 @@
 let numbers = [];
-// i=1 set default; i<=3 รับค่าสามมตัว; =>  for loop 3 round
+// i=1 set default; i<=3 รับค่าสามมตัว; => for loop 3 round
 
 for (let i = 1; i <= 3; i++) {
   let input = prompt(`Input number ${i} :`); //${i} นับจำนวนครั้ง
@@ -7,6 +7,23 @@ for (let i = 1; i <= 3; i++) {
   numbers.push(number);
 }
 
-//.sort เรียงจาก max -> min
+//.sort เรียงarrayจาก max -> min
 numbers.sort((a, b) => b - a);
 alert(numbers.join(",")); //แปลง array -> string ,คั่นระหว่าง number
+
+
+
+//Sol2
+// let numbers = [];
+// for (let i = 1; i <= 3; i++) {
+//     let input = prompt(`Input number ${i} :`);
+//     let number = Number(input);
+
+//     if(isNaN(number)) {
+//         alert("Invalid input");
+//     }else{
+//         numbers.push(number);
+//     }
+// }
+// numbers.sort((a, b) => b - a);
+// alert(numbers.join(","));
